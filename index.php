c4c3e4ca0270095f65d6e8b8181680e442064840
owokomoses <owokomose@gmail.com>
2024-05-06 19:09:03 +0300

    Addition of two numbers

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
+<!-- introduction to php -->
 <?php
-echo "Hello everyone i am back from my holiday";
+//echo "Hello everyone i am back from my holiday";
+
+// php program that adds two numbers
+// function for addition
+function addNumbers($num1, $num2) {
+    return $num1 + $num2;
+}
+
+// input
+$num1 = 10;
+$num2 = 10;
+
+// sum of numbers
+$sum = addNumbers($num1, $num2);
+
+// result
+echo "The sum is: $sum";
+?>
