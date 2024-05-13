fdf0467979539e1159f3e1e6aadfae808db3b88c
owokomoses <owokomose@gmail.com>
2024-05-13 14:29:18 +0300

    finding balance

--- index.php
+++ index.php
@@ -4,17 +4,21 @@
 
 // php program that adds two numbers
 // function for addition
-function addNumbers($num1, $num2) {
-    return $num1 + $num2;
-}
+//function addNumbers($num1, $num2) {
+    //return $num1 + $num2;
+//}
 
 // input
-$num1 = 10;
-$num2 = 10;
+//$num1 = 10;
+//$num2 = 10;
 
 // sum of numbers
-$sum = addNumbers($num1, $num2);
+//$sum = addNumbers($num1, $num2);
 
 // result
-echo "The sum is: $sum";
+//echo "The sum is: $sum";
+$deposit = 40000;
+$withdrawal = 5000;
+$balance = $deposit - $withdrawal;
+echo $balance;
 ?>
