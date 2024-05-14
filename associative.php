c6d9cae4a7c6bf47d08c4340e6544874fc3ed8c7
owokomoses <owokomose@gmail.com>
2024-05-14 15:05:12 +0300

    updated code with for each loop

--- associative.php
+++ associative.php
@@ -1,4 +1,8 @@
 <?php
-$user_input = array('username' => "John", 'email' => "John@gmail.com",  'password' =>"123");
-echo $user_input['username']
+//example 1 associative array
+$user_input = array('username' => "John " , 'email' => "John@gmail.com " ,  'password' =>"123");
+// echo $user_input['username']
+foreach ($user_input as $user) {
+    echo $user;
+}
 ?>
--- indexed.php
+++ indexed.php
@@ -1,4 +1,8 @@
 <?php
-$counties = array("Nairobi", "Mombasa", "Nakuru");
-echo $counties[0]
+//example 1 indexed array
+$counties = array("Nairobi, " , "Mombasa, " , "Nakuru ");
+// echo $counties[0];
+foreach ($counties as $county) {
+    echo $county;
+}
 ?>
