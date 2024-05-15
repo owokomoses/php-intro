f550098b562cdc970678535383037bd61a2222c6
owokomoses <owokomose@gmail.com>
2024-05-15 15:04:35 +0300

    added navigation elements

--- lesson3/navbar.php
+++ lesson3/navbar.php
@@ -1,11 +1,4 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
+
     <!-- Navigation -->
     <nav>
         <ul>
@@ -17,5 +10,3 @@
             <li><a href="signup.php">Signup</a></li>
         </ul>
     </nav>
-</body>
-</html>
